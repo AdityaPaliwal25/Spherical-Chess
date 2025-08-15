e27887cf07ec97e679d009817f6592436c8fa174
KrishnaSonawane8008 <krishnasonawane1624@gmail.com>
2025-08-16 01:03:14 +0530

    Removed a few things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1609,6 +1609,11 @@ function animate(t=0){
     renderer.render( scene, camera );
 }
 
+window.addEventListener("resize", function(){
+    camera.aspect=window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
   return (<div></div>);
 }
